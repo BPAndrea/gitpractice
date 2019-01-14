efda31841df03fdeaac29d74e80629e749c3ed96
Akos Makovics <akos.makovics@gmail.com>
2019-01-14 11:50:38 +0100

    b1

---
+++ src/com/greenfox/count.java
@@ -0,0 +1,13 @@
+package com.greenfox;
+
+public class count {
+
+  public static int printAge() {
+    int age = 20;
+    return age;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(printAge());
+  }
+}
