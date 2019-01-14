8a419607faa8fc11dce313ad55e429edc0b03115
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-01-14 11:28:38 +0100

    a1

--- src/com/greenfox/Main.java
+++ src/com/greenfox/Main.java
@@ -1,8 +1,11 @@
 package com.greenfox;
 
 public class Main {
+  public static void greet(){
+    System.out.println("Bonnyai-Pap Andrea");
+  }
 
     public static void main(String[] args) {
-	// write your code here
+	    greet();
     }
 }
